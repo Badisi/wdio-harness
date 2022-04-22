bfcc7526f904c85ade27ff81b3d628aed486ecae
Badisi <badisi84@gmail.com>
2022-04-22 22:55:28 +0200

    feat(ng13): add support for angular v13

--- projects/tests-e2e/wdio.config.ts
+++ projects/tests-e2e/wdio.config.ts
@@ -46,6 +46,11 @@ export const config: WebdriverIO.Config = {
         defaultTimeoutInterval: debug ? (24 * 60 * 60 * 1000) : 60000
     },
     autoCompileOpts: {
+        tsNodeOpts: {
+            moduleTypes: {
+                './**/*.e2e.ts': 'esm'
+            }
+        },
         tsConfigPathsOpts: {
             baseUrl: './',
             paths: {
