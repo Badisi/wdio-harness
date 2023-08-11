68942b5fb724675497476639924466bf545e7708
Badisi <badisi84@gmail.com>
2023-08-11 14:20:13 +0200

    fix(ci): remove deprecated chromedriver service

--- projects/tests-e2e/wdio.config.ts
+++ projects/tests-e2e/wdio.config.ts
@@ -61,7 +61,6 @@ export const config: Options.Testrunner = {
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
-    services: ['chromedriver'],
     framework: 'jasmine',
     reporters: ['spec'],
     jasmineOpts: {
