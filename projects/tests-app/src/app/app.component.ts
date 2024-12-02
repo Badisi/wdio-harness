049575ca19d00aad393e672fef96d73cc6b2d808
Badisi <badisi84@gmail.com>
2024-12-02 23:38:08 +0100

    feat(test-app): migrate to standalone

--- projects/tests-app/src/app/app.component.ts
+++ projects/tests-app/src/app/app.component.ts
@@ -1,9 +1,18 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import { MatOption, MatSelect } from '@angular/material/select';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    standalone: false
+    imports: [
+        NgFor, MatLabel, MatButton, MatSelect, MatInput, MatFormField, MatOption,
+        FormsModule, MatDatepickerModule
+    ]
 })
 export class AppComponent {
     public foods: string[] = ['Steak', 'Pizza', 'Tacos'];
--- projects/tests-app/src/main.ts
+++ projects/tests-app/src/main.ts
@@ -1,6 +1,11 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppComponent } from './app/app.component';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+    providers: [
+        provideAnimations(),
+        provideNativeDateAdapter()
+    ]
+}).catch(err => console.error(err));
